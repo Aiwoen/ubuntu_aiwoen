8f962874266a649c7a4703d11dc54fbb4e60554e
Aiwoen <52808743+Aiwoen@users.noreply.github.com>
2022-05-21 12:16:07 +0800

    Debug

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #}
 DATABASES = {
     'default': {
-        'ENGINE': ‘django.db.backends.mysql’,
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysite', #here is import
         'USER': 'aiwoen',
         'PASSWORD': '2959',
