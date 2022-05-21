f5b9d35686102111b28b6336e3288fe6860b4eab
Aiwoen <52808743+Aiwoen@users.noreply.github.com>
2022-05-21 14:43:22 +0800

    Debug

--- mysite/two/views.py
+++ mysite/two/views.py
@@ -26,8 +26,8 @@ def get_student(request):
 
     context = {
     
-        "hobby" : "test"
-    
+        "hobby":"test"
+        "student":students
     }
 
     return render(request, 'student.html', context=context)
