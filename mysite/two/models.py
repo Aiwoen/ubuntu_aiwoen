ac25bcc98c6cf8e97cab2e5da3723631d52b1e39
Aiwoen <52808743+Aiwoen@users.noreply.github.com>
2022-05-21 12:44:37 +0800

    Debug

--- mysite/two/models.py
+++ mysite/two/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Student(models.Model):
-    s_name - models.CharField(max_length=16)
+    s_name = models.CharField(max_length=16)
     s_age = models.IntegeField(default=1)
     
 # Create your models here.
