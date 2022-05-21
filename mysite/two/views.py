f0c8bc104c67a1e2d3b0c1284d47adba5fe75e11
Aiwoen <52808743+Aiwoen@users.noreply.github.com>
2022-05-21 12:50:30 +0800

    資料庫新增資料

--- mysite/two/views.py
+++ mysite/two/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from Two.models import Student
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("two index")
+    
+def add_student(request):
+
+    student = Student()
+    student.s_name = 'Jerry'
+    sturdent.save()
+
+    return HttpResponse("Add Success")
     
