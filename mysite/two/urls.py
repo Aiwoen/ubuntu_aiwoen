1aeadc08f7083aef04055f7f353b9432b40fa79c
Aiwoen <52808743+Aiwoen@users.noreply.github.com>
2022-05-25 16:18:54 +0800

    Debug

--- mysite/two/urls.py
+++ mysite/two/urls.py
@@ -6,9 +6,8 @@ urlpatterns = [
     path('index/', views.index),
     path('addstudent/',views.add_student),
     path('getstudent/',views.get_student),
-    path('updatestudent',views.update_student),
-    path('deletestudent',views.delete_student),
-    path('addstudents/'),views.add_students),
-    
+    path('updatestudent/',views.update_student),
+    path('deletestudent/',views.delete_student),
+    path('addstudents/',views.add_students),
     
 ]
